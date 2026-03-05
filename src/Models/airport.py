2d72e7d55a6f83a69b2bf443ac462856341e10b7
Alba Segura <m1ss.sunrse@gmail.com>
2026-03-05 14:50:46 +0100

    fix: Allow lat and lon attributes in Airport model to be optional

--- src/Models/airport.py
+++ src/Models/airport.py
@@ -1,11 +1,14 @@
+from __future__ import annotations
+
 from pydantic import BaseModel
 
 from .flight import Flight
+
 
 class Airport(BaseModel):
     name: str
     iata: str
     icao: str
-    lat: float
-    lon: float 
+    lat: float | None = None
+    lon: float | None = None
     flights_list: list[Flight]
